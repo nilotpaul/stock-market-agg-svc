f11dde92420bb964677ffc1205a111c4ab473df1
Nilotpaul Nandi <nilotpaul.nandi@gmail.com>
2026-06-19 10:56:18 +0000

    perf: small changes

--- server/server.go
+++ server/server.go
@@ -26,6 +26,7 @@ func (s *APIServer) Start() error {
 	s.registerAPIRoutes()
 
 	fmt.Printf("started at %s\n", s.listenAddr)
+	fmt.Printf("vist http://localhost%s\n", s.listenAddr)
 	return http.ListenAndServe(s.listenAddr, nil)
 }
 
